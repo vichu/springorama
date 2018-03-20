1ce1cf0fab54ace1ca04085b8a7935284224289f
Viswanathan Ranganathan <vranganathan@art.com>
2018-03-20 14:33:16 -0700

    Added AmazonS3ResourcesExample project.

---
+++ AmazonS3ResourcesExample/src/main/java/com/indywiz/springorama/AmazonS3ResourcesExample/AmazonS3ResourcesExampleApplication.java
@@ -0,0 +1,12 @@
+package com.indywiz.springorama.AmazonS3ResourcesExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonS3ResourcesExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonS3ResourcesExampleApplication.class, args);
+	}
+}
---
+++ AmazonS3ResourcesExample/src/test/java/com/indywiz/springorama/AmazonS3ResourcesExample/AmazonS3ResourcesExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.indywiz.springorama.AmazonS3ResourcesExample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmazonS3ResourcesExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

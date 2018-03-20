9e69d8753b2489d8429b7e66b1e30bd6ada78e35
Viswanathan Ranganathan <vranganathan@art.com>
2018-03-20 14:24:03 -0700

    Added degraph module and .gitignore

---
+++ degraphexample/src/main/java/com/indywiz/springorama/degraphexample/DegraphExampleApplication.java
@@ -0,0 +1,12 @@
+package com.indywiz.springorama.degraphexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegraphExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DegraphExampleApplication.class, args);
+	}
+}

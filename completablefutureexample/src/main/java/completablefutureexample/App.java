e7d23ede5a771a5c5e3fa1b70efcd5fe4e7d68ef
Viswanathan R <vranganathan2@uh.edu>
2018-04-09 22:30:24 -0700

    Committing completable-future: future example

---
+++ completablefutureexample/src/main/java/completablefutureexample/App.java
@@ -0,0 +1,32 @@
+package completablefutureexample;
+
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class App {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        //Future example
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Future<Integer> future = executorService.submit(() -> {
+            System.out.println("Executing a task.");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return new Random().nextInt(20);
+        });
+
+        while (!future.isDone()) {
+            System.out.println("Task not yet done...");
+            Thread.sleep(100);
+        }
+        System.out.println("The future is resolved. Value generated = "+future.get());
+        executorService.shutdown();
+
+    }
+}

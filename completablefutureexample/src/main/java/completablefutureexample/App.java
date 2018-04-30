2f14edbc46b99c7ac8f95fcac17d703746bdf3bc
Viswanathan Ranganathan <vranganathan@art.com>
2018-04-30 10:43:02 -0700

    feat(springintegration): changes to pom file
    Changes to the pom file to add a tiny bit of documentation. Committing the completable future
    changes.

--- completablefutureexample/src/main/java/completablefutureexample/App.java
+++ completablefutureexample/src/main/java/completablefutureexample/App.java
@@ -1,32 +1,48 @@
 package completablefutureexample;
 
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 
 public class App {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        //Future example
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        Future<Integer> future = executorService.submit(() -> {
-            System.out.println("Executing a task.");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return new Random().nextInt(20);
-        });
+  public static void main(String[] args) {
 
-        while (!future.isDone()) {
-            System.out.println("Task not yet done...");
-            Thread.sleep(100);
-        }
-        System.out.println("The future is resolved. Value generated = " + future.get());
-        executorService.shutdown();
+    CompletableFuture<Integer> completableFuture = new CompletableFuture<>();
 
-    }
+    //Define your flow
+    completableFuture.thenApply((integer) -> {
+      int randomNumber = 0;
+      try {
+        randomNumber = generateRandomNumber();
+        System.out.println("Adding " + integer + " to the random number " + randomNumber);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      System.out.println(
+          "Generated random number=" + randomNumber + " from thread : " + Thread.currentThread()
+              .getName());
+      return randomNumber + integer;
+    }).thenApplyAsync(integer -> {
+      System.out.println("Got result from the first thread.");
+      return integer + 500;
+    }, new ForkJoinPool()).thenAccept(integer -> {
+      System.out.println("Added 500 to the result. Terminating the completable future.");
+    });
+
+    //Start the processing
+    completableFuture.complete(10);
+
+  }
+
+  private static int generateRandomNumber() throws InterruptedException {
+    int number = new Random().nextInt(100);
+    System.out.println("Sleeping for " + number + "ms");
+    Thread.sleep(number);
+    return number;
+  }
 }

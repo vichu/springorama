960f0c04656249e449af5e1ce0cadbeec6577533
Vish <vranganathan@twilio.com>
2018-12-22 11:10:22 -0800

    Step-3: Create database service from DbProvider

---
+++ phantom-scala-example/src/main/scala/com/indywiz/springorama/phantom/example/database/service/UserDatabaseService.scala
@@ -0,0 +1,25 @@
+package com.indywiz.springorama.phantom.example.database.service
+
+import java.util.UUID
+
+import com.indywiz.springorama.phantom.example.database.model.User
+import com.indywiz.springorama.phantom.example.database.provider.UserDbProvider
+import com.outworkers.phantom.dsl._
+
+import scala.concurrent.Future
+
+trait UserDatabaseService extends UserDbProvider {
+
+  // Orchestrate your low level queries appropriately.
+  def createUser(uuid: UUID, firstName: String, lastName: String, email:String): Future[Boolean] =
+    database.userByIdInstance.createUserById(uuid, firstName, lastName, email)
+    .flatMap(_ => {
+      database.userByFirstName.createUserByUserName(uuid, firstName, lastName, email)
+        .map(rs => if(rs.wasApplied) true else false)
+    })
+
+  def selectUserById(uuid: UUID): Future[Option[User]] = database.userByIdInstance.getUserById(uuid)
+
+  def selectUserByFirstName(firstName: String): Future[Option[User]] = database.userByFirstName.getUserByFirstName(firstName)
+
+}

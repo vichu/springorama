f0f0c3ac4158fea797dbbeef833d155a652aa5af
Vish <vranganathan@twilio.com>
2020-08-09 00:26:35 -0700

    Defined our endpoint

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/sayHi")
+	public String sayHiFromSpringBoot(@RequestParam("name") final String name) {
+		return "Hi from " + name;
 	}
 
 }
